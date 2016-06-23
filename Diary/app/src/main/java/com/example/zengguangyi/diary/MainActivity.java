ba0410cd2237771f5533037cd8c3cf97605dded7
zengguangyi <m13680538036@163.com>
2016-06-23 21:23:22 +0800

    another Diary basically completed

--- Diary/app/src/main/java/com/example/zengguangyi/diary/MainActivity.java
+++ Diary/app/src/main/java/com/example/zengguangyi/diary/MainActivity.java
@@ -21,14 +21,17 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-//    Button btn_add;
-//    Button btn_refresh;
+
     ListView listView;
     ArrayAdapter<String> adapter;
+    int listnum=0;
 
-    private String[] listdata = {"apple","banana","orange"};
+//    private String[] listdata = {"01","02","03","04","05","06","07","08","09","10"};
+    private ArrayList listdata = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,22 +39,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*SharedPreferences获取数据*/
-//        btn_add = (Button)findViewById(R.id.btnAdd);
-//        btn_add.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
-//                String name = sp.getString("TITLE", "");
-//                Log.d("MyA", "TITLE is " + name);
-//            }
-//        });
-
-
-        /*SharedPreferences获取日记TITLE*/
+        /*获取SharedPreferences的数据赋值给数组listdata*/
         SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
-        String title = sp.getString("TITLE", "");
-        listdata[0] = title;
+        while(true){
+            if (sp.getString("TITLE"+listnum, "") == "")break;//没有title
+//            listdata[listnum] = sp.getString("TITLE"+listnum, "");
+            listdata.add(sp.getString("TITLE"+listnum, "")); //追加日记title
+            listnum++;
+        }
 
         listView = (ListView)findViewById(R.id.main_list);
         adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,listdata);
@@ -59,20 +54,15 @@ public class MainActivity extends AppCompatActivity
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Log.d("MyA", "item_position is " + position);
-//                Log.d("MyA", "item_title is " + listdata[position]);
-                Toast.makeText(MainActivity.this,listdata[position],Toast.LENGTH_SHORT).show();
+                /*活动跳转*/
+                Intent intent = new Intent(MainActivity.this,edit.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("LISTNUM",position);//把listnum列表数目告诉edit活动
+                intent.putExtras(bundle);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this,listdata[position],Toast.LENGTH_SHORT).show();
             }
         });
-
-//        btn_refresh = (Button)findViewById(R.id.main_refresh);
-//        btn_refresh.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,listdata);
-//                listView.setAdapter(adapter);
-//            }
-//        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +73,9 @@ public class MainActivity extends AppCompatActivity
 
                 /*活动跳转*/
                 Intent intent = new Intent(MainActivity.this,edit.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("LISTNUM",listnum);//把listnum列表数目告诉edit活动
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
--- Diary/app/src/main/java/com/example/zengguangyi/diary/edit.java
+++ Diary/app/src/main/java/com/example/zengguangyi/diary/edit.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ public class edit extends Activity {
     Button editBtn;
     EditText editTitle;
     EditText editContent;
+    int edit_listnum;
 
     protected void  onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,6 +26,16 @@ public class edit extends Activity {
         editContent = (EditText)findViewById(R.id.edit_content);
         editBtn = (Button)findViewById(R.id.edit_btn);
 
+        /*接收main传过来的LISTNUM*/
+        Bundle bundle = edit.this.getIntent().getExtras();
+        edit_listnum = bundle.getInt("LISTNUM");
+        Log.d("editA", "editlistnum: " + edit_listnum);
+
+        /*读取该条日记*/
+        SharedPreferences edit_sp = getSharedPreferences("data3", MODE_PRIVATE);
+        editTitle.setText(edit_sp.getString("TITLE" + edit_listnum, ""));
+        editContent.setText(edit_sp.getString("CONTENT" + edit_listnum, ""));
+
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,16 +43,13 @@ public class edit extends Activity {
                 String content = editContent.getText().toString();
 
                  /*SharedPreferences存储数据*/
-                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
-                editor.putString("TITLE",title);
-                editor.putString("CONTENT", content);
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_APPEND).edit();
+                editor.putString("TITLE" + edit_listnum, title);
+                editor.putString("CONTENT" + edit_listnum, content);
                 editor.commit();
 
                 /*跳转主活动*/
-                Intent intent = new Intent(edit.this,MainActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putInt("ID",102);
-                intent.putExtras(bundle);
+                Intent intent = new Intent(edit.this, MainActivity.class);
                 startActivity(intent);
             }
         });
